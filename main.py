f196f69456d198caccd69d8f754d4729b305aa1e
Sayuri Herencia <sayuri.herencia@ucsp.edu.pe>
2025-08-08 18:37:28 -0500

    Tratamiento y depuración de datos Satisfactorios

--- main.py
+++ main.py
@@ -3,5 +3,96 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv("dataset_banco.csv")
-print(data.shape)
+#print(data.shape)
+data.info()
+
+data.dropna(inplace=True)
+#data.info()
+cols_cat = ['job', 'marital', 'education', 'default', 'housing','loan', 'contact', 'month', 'poutcome', 'y']
+for col in cols_cat: print(f'Columna {col}: {data[col].nunique()} subniveles')
+
+print(f'Tamaño del set antes de eliminar las filas repetidas: {data.shape}')
+data.drop_duplicates(inplace=True)
+print(f'Tamaño del set después de eliminar las filas repetidas: {data.shape}')
+
+
+
+cols_num = ['age', 'balance', 'day', 'duration', 'campaign','pdays', 'previous']
+
+fig, ax = plt.subplots(nrows=7, ncols=1, figsize=(8,30))
+fig.subplots_adjust(hspace=0.5)
+
+for i, col in enumerate(cols_num):
+    sns.boxplot(x=col, data=data, ax=ax[i])
+    ax[i].set_title(col)
+
+
+print(f'Tamaño del set antes de eliminar registros de edad: {data.shape}')
+data = data[data['age']<=100]
+print(f'Tamaño del set después de eliminar registros de edad: {data.shape}')
+
+
+print(f'Tamaño del set antes de eliminar registros de duración: {data.shape}')
+data = data[data['duration']>0]
+print(f'Tamaño del set después de eliminar registros de duración: {data.shape}')
+
+
+print(f'Tamaño del set antes de eliminar registros de previous: {data.shape}')
+data = data[data['previous']<=100]
+print(f'Tamaño del set después de eliminar registros de previous: {data.shape}')
+
+cols_cat = ['job', 'marital', 'education', 'default', 'housing','loan', 'contact', 'month', 'poutcome', 'y']
+
+
+
+
+fig, ax = plt.subplots(nrows=10, ncols=1, figsize=(10,30))
+fig.subplots_adjust(hspace=1)
+for i, col in enumerate(cols_cat): 
+    sns.countplot(x=col, data=data, ax=ax[i]) 
+    ax[i].set_title(col) 
+    ax[i].tick_params(axis='x', rotation=30)
+
+
+
+
+
+for column in data.columns:
+    if column in cols_cat:
+        data[column] = data[column].str.lower()
+
+fig, ax = plt.subplots(nrows=10, ncols=1, figsize=(10,30))
+fig.subplots_adjust(hspace=1)
+for i, col in enumerate(cols_cat):
+    sns.countplot(x=col, data=data, ax=ax[i])
+    ax[i].set_title(col)
+    ax[i].tick_params(axis='x', rotation=30)
+#    ax[i].set_xticklabels(ax[i].get_xticklabels(),rotation=30)
+
+
+
+print(data['job'].unique())
+data['job'] = data['job'].str.replace('admin.','administrative', regex=False)
+print(data['job'].unique())
+
+print(data['marital'].unique())
+data['marital'] = data['marital'].str.replace('div.','divorced', regex=False)
+print(data['marital'].unique())
+
+print(data['education'].unique())
+data['education'] = data['education'].str.replace('sec.','secondary', regex=False)
+data.loc[data['education']=='unk','education'] = 'unknown'
+print(data['education'].unique())
+
+print(data['contact'].unique())
+data.loc[data['contact']=='phone','contact'] = 'telephone'
+data.loc[data['contact']=='mobile','contact'] = 'cellular'
+print(data['contact'].unique())
+
+print(data['poutcome'].unique())
+data.loc[data['poutcome']=='unk','poutcome']='unknown'
+print(data['poutcome'].unique())
+
+
+data.shape
 data.info()
